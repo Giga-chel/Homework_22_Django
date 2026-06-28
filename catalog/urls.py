8f4e6e376b58399e2cd35e99b99858a64b143754
Eugen <trushin00332211@gmail.com>
2026-06-28 18:32:44 +0500

    Доп.задание: Шаблон формы и обновление файлов

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,6 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
-    # --- НОВОЕ: Маршрут для Задания 1 ---
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/create/', views.create_product, name='create_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from .models import Product, Contact
+from .forms import ProductForm
 
 
 def home(request):
-    last_five_products = Product.objects.all().order_by('-created_at')[:5]
+    products_list = Product.objects.all().order_by('-created_at')
+
+    paginator = Paginator(products_list, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'object_list': last_five_products
+        'page_obj': page_obj,
     }
     return render(request, 'catalog/home.html', context)
 
@@ -34,3 +41,18 @@ def product_detail(request, pk):
         'product': product
     }
     return render(request, 'catalog/product_detail.html', context)
+
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:home')
+    else:
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'catalog/product_form.html', context)
