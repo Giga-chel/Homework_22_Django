4ad326cb484df33364052cda43c37f690bb0a1ca
Eugen <trushin00332211@gmail.com>
2026-07-24 01:30:47 +0500

    Задание 3 Закрытие доступа к управлению продуктами

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView, View, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Product, Contact
 from .forms import ProductForm
 
@@ -34,27 +35,25 @@ class ContactsView(View):
         return render(request, 'catalog/contacts.html', context)
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = '/'
 
-
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:home')
 
-
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:home')
