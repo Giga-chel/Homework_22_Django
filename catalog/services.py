bcb8ffb34c8598b8f400b88d2ce563a10047db0d
Eugen <trushin00332211@gmail.com>
2026-08-03 03:17:50 +0500

    Доработка шаблона и реализации низкоуровневого кеша

--- catalog/services.py
+++ catalog/services.py
@@ -7,12 +7,14 @@ def get_products_by_category(category_id):
     Сервисная функция для получения продуктов по ID категории.
     Использует низкоуровневое кеширование.
     """
-    cache_key = f'products_by_category_{category_id}'
+    cache_key = f'category_{category_id}'
 
     products = cache.get(cache_key)
 
     if products is None:
-        products = Product.objects.filter(category_id=category_id).select_related('category')
+        products = list(
+            Product.objects.filter(category_id=category_id).select_related('category')
+        )
         cache.set(cache_key, products, 60 * 15)
 
     return products
